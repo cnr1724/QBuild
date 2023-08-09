ca35ae36cfbd396c0c199829cdcaadf2876438ba
cnr1724 <cnr1724@gmail.com>
2023-08-09 16:00:02 -0400

    Initial commit

---
+++ Form2.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Qbuild
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(110, 433);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 72;
+            this.dataGridView1.RowTemplate.Height = 31;
+            this.dataGridView1.Size = new System.Drawing.Size(1173, 323);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(110, 22);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(545, 370);
+            this.treeView1.TabIndex = 1;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(910, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(283, 84);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Populate Data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(910, 259);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(283, 84);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1363, 817);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Qbuild
+{
+    public partial class Form2 : Form
+    {
+        private string connectionString = "Data Source=LAPTOP-MSRLEJ67;Initial Catalog=QbuildDB;Integrated Security=True";
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            string selectedPartName = e.Node.Text;
+            dataGridView1.Rows.Clear();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string selectComponentPartsQuery = "SELECT * FROM tblBOM WHERE ParentName = @ParentName";
+                using (SqlCommand selectComponentPartsCommand = new SqlCommand(selectComponentPartsQuery, connection))
+                {
+                    selectComponentPartsCommand.Parameters.AddWithValue("@ParentName", selectedPartName);
+                    using (SqlDataReader reader = selectComponentPartsCommand.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string componentName = reader["ComponentName"].ToString();
+                            decimal quantity = Convert.ToDecimal(reader["Quantity"]);
+
+                            using (SqlConnection partInfoConnection = new SqlConnection(connectionString))
+                            {
+                                partInfoConnection.Open();
+
+                                string selectPartInfoQuery = "SELECT * FROM tblParts WHERE Name = @Name";
+                                using (SqlCommand selectPartInfoCommand = new SqlCommand(selectPartInfoQuery, partInfoConnection))
+                                {
+                                    selectPartInfoCommand.Parameters.AddWithValue("@Name", componentName);
+                                    using (SqlDataReader partInfoReader = selectPartInfoCommand.ExecuteReader())
+                                    {
+                                        if (partInfoReader.Read())
+                                        {
+                                            string partNumber = partInfoReader["PartNumber"].ToString();
+                                            string title = partInfoReader["Title"].ToString();
+                                            string type = partInfoReader["Type"].ToString();
+                                            string item = partInfoReader["Item"].ToString();
+                                            string material = partInfoReader["Material"].ToString();
+
+                                            dataGridView1.Rows.Add(                                          
+                                                partNumber,
+                                                title,                                          
+                                                type,
+                                                item,
+                                                material
+                                            );
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    
+
+        private void BuildSubtree(TreeNode parentNode, string partName, int level)
+        {
+            if (level > 5)
+                return;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string selectChildPartsQuery = "SELECT ComponentName FROM tblBOM WHERE ParentName = @ParentName";
+                using (SqlCommand selectChildPartsCommand = new SqlCommand(selectChildPartsQuery, connection))
+                {
+                    selectChildPartsCommand.Parameters.AddWithValue("@ParentName", partName);
+                    using (SqlDataReader reader = selectChildPartsCommand.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string childPartName = reader["ComponentName"].ToString();
+                            var childNode = new TreeNode(childPartName);
+                            parentNode.Nodes.Add(childNode);
+                            BuildSubtree(childNode, childPartName, level + 1);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void BuildTreeView()
+        {
+            treeView1.Nodes.Clear();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string selectRootPartsQuery = "SELECT DISTINCT ComponentName FROM tblBOM WHERE ParentName IS NULL";
+                using (SqlCommand selectRootPartsCommand = new SqlCommand(selectRootPartsQuery, connection))
+                using (SqlDataReader reader = selectRootPartsCommand.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string rootPartName = reader["ComponentName"].ToString();
+                        var rootTreeNode = new TreeNode(rootPartName);
+                        BuildSubtree(rootTreeNode, rootPartName, 1);
+                        treeView1.Nodes.Add(rootTreeNode);
+                    }
+                }
+            }
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            dataGridView1.Columns.Add("ComponentName", "ComponentName");
+            dataGridView1.Columns.Add("PartNumber", "PartNumber");
+            dataGridView1.Columns.Add("Title", "Title");
+            dataGridView1.Columns.Add("Quantity", "Quantity");
+            dataGridView1.Columns.Add("Type", "Type");
+            dataGridView1.Columns.Add("Item", "Item");
+            dataGridView1.Columns.Add("Material", "Material");
+
+            BuildTreeView();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+}
